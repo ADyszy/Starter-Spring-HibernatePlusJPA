d03b82db63a4b41070e669a0976e6f87d15b838f
ADyszy <ADYSZY@CE11704.corp.capgemini.com>
2015-08-17 15:02:27 +0200

    repository refact.

--- spring-data-jpa/src/main/java/pl/spring/demo/repository/impl/BookRepositoryImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/repository/impl/BookRepositoryImpl.java
@@ -23,7 +23,29 @@ public class BookRepositoryImpl implements CustomBookRepository {
 	@PersistenceContext
 	private EntityManager entityManager;
 	
-	@Override
+	private void addTitleFilter(BooleanBuilder booleanBuilder, BookSearchCriteria criteria, QBookEntity bookEntity) {
+		if ( !StringUtils.isEmpty(criteria.getTitle()) ) {
+			final String title = criteria.getTitle();
+			booleanBuilder.and(bookEntity.title.startsWithIgnoreCase(title));
+		}
+	}
+	
+	private void addAuthorFilter(BooleanBuilder booleanBuilder, BookSearchCriteria criteria, QBookEntity bookEntity) {
+		if ( !StringUtils.isEmpty(criteria.getAuthor()) ) {
+			final String author = criteria.getAuthor();
+			booleanBuilder.and( bookEntity.authors.any().firstName.startsWithIgnoreCase(author)
+					.or(bookEntity.authors.any().lastName.startsWithIgnoreCase(author)));
+		}
+	}
+	
+	private void addLibraryNameFilter(BooleanBuilder booleanBuilder, BookSearchCriteria criteria, QBookEntity bookEntity) {
+		if ( !StringUtils.isEmpty(criteria.getLibraryName()) ) {
+			final String libName = criteria.getLibraryName();
+			booleanBuilder.and(bookEntity.library.name.startsWithIgnoreCase(libName));
+		}
+	}
+	
+	@Override // TODO[qstn]: to dao, service or here, in repo ?
 	public List<BookEntity> findBookByCriteria(BookSearchCriteria criteria) {		
 		final QBookEntity bookEntity = QBookEntity.bookEntity;
 		
@@ -33,19 +55,9 @@ public class BookRepositoryImpl implements CustomBookRepository {
 		if (criteria != null ) {	
 			final BooleanBuilder booleanBuilder = new BooleanBuilder();
 			
-			if ( !StringUtils.isEmpty(criteria.getTitle()) ) {
-				final String title = criteria.getTitle();
-				booleanBuilder.and(bookEntity.title.startsWithIgnoreCase(title));
-			}
-			if ( !StringUtils.isEmpty(criteria.getAuthor()) ) {
-				final String author = criteria.getAuthor();
-				booleanBuilder.and( bookEntity.authors.any().firstName.startsWithIgnoreCase(author)
-						.or(bookEntity.authors.any().lastName.startsWithIgnoreCase(author)));
-			}
-			if ( !StringUtils.isEmpty(criteria.getLibraryName()) ) {
-				final String libName = criteria.getLibraryName();
-				booleanBuilder.and(bookEntity.library.name.startsWithIgnoreCase(libName));
-			}
+			addTitleFilter(booleanBuilder, criteria, bookEntity);
+			addAuthorFilter(booleanBuilder, criteria, bookEntity);
+			addLibraryNameFilter(booleanBuilder, criteria, bookEntity);
 			
 			query.where(booleanBuilder);
 		}
