52284e0decd05486e3469062f8a32683c4c82ecc
ADyszy <ADYSZY@CE11704.corp.capgemini.com>
2015-08-17 08:29:03 +0200

    findBookByCriteria moved to DAO.

--- spring-data-jpa/src/main/java/pl/spring/demo/dao/impl/BookDaoImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/dao/impl/BookDaoImpl.java
@@ -1,18 +1,57 @@
 package pl.spring.demo.dao.impl;
 
+import pl.spring.demo.criteria.BookSearchCriteria;
 import pl.spring.demo.dao.BookDao;
 import pl.spring.demo.entity.BookEntity;
+import pl.spring.demo.entity.QBookEntity;
 
 import javax.persistence.TypedQuery;
+
+import org.springframework.util.StringUtils;
+
+import com.mysema.query.BooleanBuilder;
+import com.mysema.query.jpa.HQLTemplates;
+import com.mysema.query.jpa.impl.JPAQuery;
+
 import java.util.List;
 
-public class BookDaoImpl extends AbstractDao<BookEntity, Long> implements BookDao {
+public class BookDaoImpl extends AbstractDao<BookEntity, Long>implements BookDao {
 
-    @Override
-    public List<BookEntity> findBookByTitle(String title) {
-        TypedQuery<BookEntity> query = entityManager.createQuery(
-                "select book from BookEntity book where upper(book.title) like concat(upper(:title), '%')", BookEntity.class);
-        query.setParameter("title", title);
-        return query.getResultList();
-    }
+	@Override
+	public List<BookEntity> findBookByTitle(String title) {
+		TypedQuery<BookEntity> query = entityManager.createQuery(
+				"select book from BookEntity book where upper(book.title) like concat(upper(:title), '%')",
+				BookEntity.class);
+		query.setParameter("title", title);
+		return query.getResultList();
+	}
+
+	@Override
+	public List<BookEntity> findBookByCriteria(BookSearchCriteria criteria) {
+		final QBookEntity bookEntity = QBookEntity.bookEntity;
+
+		final JPAQuery query = new JPAQuery(entityManager, HQLTemplates.DEFAULT);
+		query.from(bookEntity);
+
+		if (criteria != null) {
+			final BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+			if (!StringUtils.isEmpty(criteria.getTitle())) {
+				final String title = criteria.getTitle();
+				booleanBuilder.and(bookEntity.title.startsWithIgnoreCase(title));
+			}
+			if (!StringUtils.isEmpty(criteria.getAuthor())) {
+				final String author = criteria.getAuthor();
+				booleanBuilder.and(bookEntity.authors.any().firstName.startsWithIgnoreCase(author)
+						.or(bookEntity.authors.any().lastName.startsWithIgnoreCase(author)));
+			}
+			if (!StringUtils.isEmpty(criteria.getLibraryName())) {
+				final String libName = criteria.getLibraryName();
+				booleanBuilder.and(bookEntity.library.name.startsWithIgnoreCase(libName));
+			}
+
+			query.where(booleanBuilder);
+		}
+		return query.listResults(bookEntity).getResults();
+	}
 }
--- spring-data-jpa/src/test/java/pl/spring/demo/dao/BookDaoImplTest.java
+++ spring-data-jpa/src/test/java/pl/spring/demo/dao/BookDaoImplTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import pl.spring.demo.criteria.BookSearchCriteria;
 import pl.spring.demo.entity.BookEntity;
 
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "CommonDaoTest-context.xml")
@@ -42,4 +45,88 @@ public class BookDaoImplTest {
         assertFalse(booksEntity.isEmpty());
         assertEquals("Pierwsza książka", booksEntity.get(0).getTitle());
     }
+    
+    
+    @Test
+	public void testShouldFindBookWithOnlyTitleCriteria() {
+		// given
+		BookSearchCriteria criteria = new BookSearchCriteria.CriteriaBuilder()
+				.title("Pierwsza")
+				.build();
+		// when
+		List<BookEntity> result = bookDao.findBookByCriteria(criteria);
+		
+		// then
+		assertNotNull(result);
+		assertFalse(result.isEmpty());
+		assertTrue(result.size() == 1);
+	}
+
+	@Test
+	public void testShouldFindBookWithOnlyAuthorCriteria() {
+		// given
+		BookSearchCriteria criteria = new BookSearchCriteria.CriteriaBuilder()
+				.author("Jan")
+				.build();
+		// when
+		List<BookEntity> result = bookDao.findBookByCriteria(criteria);
+		for (BookEntity bookEntity : result) {
+			System.out.println(bookEntity.getTitle());
+		}
+		// then
+		assertNotNull(result);
+		assertFalse(result.isEmpty());
+	}
+
+	@Test
+	public void testShouldFindBookWithOnlyLibraryNameCriteria() {
+		// given
+		BookSearchCriteria criteria = new BookSearchCriteria.CriteriaBuilder()
+				.libraryName("bib")
+				.build();
+		// when
+		List<BookEntity> result = bookDao.findBookByCriteria(criteria);
+		for (BookEntity bookEntity : result) {
+			System.out.println(bookEntity.getTitle());
+		}
+		// then
+		assertNotNull(result);
+		assertFalse(result.isEmpty());
+	}
+
+	@Test
+	public void testShouldFindAllBooksBecauseOfNoCriteria() {
+		// given
+		BookSearchCriteria criteria = new BookSearchCriteria.CriteriaBuilder()
+				.build();
+		// when
+		List<BookEntity> result = bookDao.findBookByCriteria(criteria);
+		for (BookEntity bookEntity : result) {
+			System.out.println(bookEntity.getTitle());
+		}
+		// then
+		assertNotNull(result);
+		assertFalse(result.isEmpty());
+	}
+
+	@Test
+	public void testShouldFindParticularBooko() {
+		// given
+		BookSearchCriteria criteria = new BookSearchCriteria.CriteriaBuilder()
+				.author("Jan")
+				.title("Pierwsza")
+				.libraryName("Biblioteka Miejska")
+				.build();
+		// when
+		List<BookEntity> result = bookDao.findBookByCriteria(criteria);
+		for (BookEntity bookEntity : result) {
+			System.out.println(bookEntity.getTitle());
+		}
+		// then
+		assertNotNull(result);
+		assertFalse(result.isEmpty());
+		assertTrue(result.size() == 1);
+	}
+
+    
 }
